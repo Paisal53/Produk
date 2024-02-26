5d198482566d5ccc071f55f91f0323b2457528db
isalpaisal040@gmail.com <isalpaisal040@gmail.com>
2024-02-26 11:23:16 +0700

    ganti

--- main.js
+++ main.js
@@ -47,14 +47,14 @@ export function formatAngka(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 }
 
-export function tambahProduk(nama,harga,stok) {
+export async function tambahProduk(nama,harga,stok) {
 try {
-  const dokRef = await addDoc(collection(db,'produk'),{
+  const dokRef = await addDoc(collection(db,"produk"),{
     nama:nama,
     harga:harga,
     stok:stok
     });
-    console.log('berhasil menambah produk'+dokRef.id;)
+    console.log('berhasil menambah produk'+dok)
   }catch (e) {
     console.log('Gagal menambah produk' + e);
     }
